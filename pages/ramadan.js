8c7bb14c0acccf94c565694266b4eb2b98046e32
chk7964 <chk7964@yahoo.com>
2023-01-30 22:39:35 +0530

    v

--- pages/ramadan.js
+++ pages/ramadan.js
@@ -39,25 +39,25 @@ const Home = () => {
 
   return (
     <>
-    <div className=" bg-rose-400 h-screen">
-      <Head>
-        <title>Ramadan Countdown Clock</title>
-        <link rel="icon" href="/favicon.ico" />
-        {/* <link rel="stylesheet" href="/css/bg.css"></link> */}
-      </Head>
+      <div className=" bg-rose-400 h-screen">
+        <Head>
+          <title>Ramadan Countdown Clock</title>
+          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="stylesheet" href="/css/bg.css"></link> */}
+        </Head>
 
-      {partyTime ? (
-        <>
-        <div className="text-center pt-[40vh] mb-48">
-          <h1 className="font-semibold text-4xl">Happy Ramadan</h1>
-          </div>
-          {/* <video autoPlay loop muted>
+        {partyTime ? (
+          <>
+            <div className="text-center pt-[40vh] mb-48">
+              <h1 className="font-semibold text-4xl">Happy Ramadan</h1>
+            </div>
+            {/* <video autoPlay loop muted>
             <source src="/party.mp4" />
           </video> */}
-        </>
-      ) : (
-        <>
-         {/* <div className="flex border border-red-400 justify-center text-center w-max px-p ">
+          </>
+        ) : (
+          <>
+            {/* <div className="flex border border-red-400 justify-center text-center w-max px-p ">
               <div className=" border border-red-400 m-2 p-2 ">
                 <span className="time">{days} </span>
                 <span className="label">Days</span>
@@ -78,37 +78,37 @@ const Home = () => {
                 <span className="label">Seconds</span>
               </div>
               </div> */}
-<div className="text-center ">
-  <h1 className="font-bold text-3xl">Ramadan Countdown</h1>
-</div>
-              <div id="countdown" className="h-auto m-auto flex items-center justify-center pt-[30vh] animate-pulse">
-    <ul>
-      <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="days" className="block">{days}</span>days</li>
-      <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="hours" className="block">{hours}</span>Hours</li>
-      <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="minutes" className="block">{minutes}</span>Minutes</li>
-      <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="seconds" className="block">{seconds}</span>Seconds</li>
-    </ul>
-  </div>
+            <div className="text-center ">
+              <h1 className="font-bold text-3xl">Ramadan Countdown</h1>
+            </div>
+            <div id="countdown" className="h-auto m-auto flex items-center justify-center pt-[30vh] animate-pulse">
+              <ul>
+                <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="days" className="block">{days}</span>days</li>
+                <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="hours" className="block">{hours}</span>Hours</li>
+                <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="minutes" className="block">{minutes}</span>Minutes</li>
+                <li className="font-bold skew-y-6 stroke-yellow-400 uppercase list-none inline-block p-3 text-center text-lg sm:text-lg md:text-3xl lg:text-6xl"><span id="seconds" className="block">{seconds}</span>Seconds</li>
+              </ul>
+            </div>
 
-<div className="mt-48 mb-4 pb-4 border border-lime-400 text-center">
-<p className="p-4">Ramadan Countdown</p>
+            <div className="mt-48 mb-4 pb-4 border border-lime-400 text-center">
+              <p className="p-4">Ramadan Countdown</p>
 
-</div>
+            </div>
 
 
-           
-    <style jsx>{`
+
+            <style jsx>{`
     
       
       `}</style>
-        
-        </>
-      )}
-      <div className="mb-4 border border-lime-400 text-center">
-<p className="p-4">Advertise</p>
 
-</div>
-    </div>
+          </>
+        )}
+        <div className="mb-4 border border-lime-400 text-center">
+          <p className="p-4">Advertise</p>
+
+        </div>
+      </div>
     </>
   );
 };
